aef21f08b3985a587c7353905d12a6dd77d99775
priyankaV311 <pv3112003@gmail.com>
2024-09-15 21:15:51 +0530

    added a search component

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchBox from './components/SearchBox'
+
+
+function App() {
+  return (
+    <div>
+      <SearchBox/>
+      </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/SearchBox.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+function SearchBox() {
+  return (
+    <div>
+        <h1>Search for the weather</h1>
+        <form action="">
+        <TextField id="outlined-basic" label="City name" variant="outlined" />
+        </form>
+    </div>
+  )
+}
+
+export default SearchBox
